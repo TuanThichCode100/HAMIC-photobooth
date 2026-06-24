83ff5f60e4663a716a4f417d9a3bf6845a7ed8b9
TuanThichCode100 <24001719@hus.edu.vn>
2026-06-25 00:52:01 +0700

    Clean codebase at branch main

--- types.ts
+++
@@ -1,9 +0,0 @@
-
-import { FrameCoord } from './constants';
-
-export interface PhotoFrame {
-  topic: string;
-  frame_content: string; // URL to the transparent PNG frame
-  number: number;
-  coords: FrameCoord[];
-}
